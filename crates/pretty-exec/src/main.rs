ddf7501d350c365b45fb37a558552c528ddc7968
khai96_ <hvksmr1996@gmail.com>
2020-05-19 16:51:45 +0700

    Complete pretty-exec

--- crates/pretty-exec/src/main.rs
+++ crates/pretty-exec/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("hello");
+    let status = match pretty_exec_lib::exec().and_then(|status| {
+        status
+            .code()
+            .ok_or_else(|| "Failed to get status code".to_owned())
+    }) {
+        Ok(status) => status,
+        Err(error) => {
+            eprintln!("{}", error);
+            1
+        }
+    };
+
+    std::process::exit(status);
 }
