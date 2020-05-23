828a81e98d57b3605611586d1165b2e9a9ff08e8
khai96_ <hvksmr1996@gmail.com>
2020-05-23 18:18:45 +0700

    Create clippy.sh and fmt.sh

---
+++ fmt.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+if [ "$FMT_UPDATE" = 'true' ]; then
+  cargo_fmt_flag=()
+elif [ "$FMT_UPDATE" = 'false' ] || [ "$FMT_UPDATE" = '' ]; then
+  cargo_fmt_flag=('--check')
+fi
+
+exec cargo fmt -- "${cargo_fmt_flag[@]}"
