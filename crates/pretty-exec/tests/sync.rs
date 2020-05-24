7572e7487a8dcc9c9282c355b88fe8c9d041ca6d
khai96_ <hvksmr1996@gmail.com>
2020-05-24 14:39:22 +0700

    refactor: Use macro

--- crates/pretty-exec/tests/sync.rs
+++ crates/pretty-exec/tests/sync.rs
@@ -16,28 +16,22 @@ struct SyncJson {
     pub version: String,
 }
 
-fn get_json_version() -> String {
-    include_str!("../../../sync.json")
-        .pipe(serde_json::from_str::<SyncJson>)
-        .pipe(Result::unwrap)
-        .pipe(|x: SyncJson| x.version)
+macro_rules! test_version {
+    ($name:ident, $path:literal) => {
+        #[test]
+        fn $name() {
+            let json_version = include_str!("../../../sync.json")
+                .pipe(serde_json::from_str::<SyncJson>)
+                .pipe(Result::unwrap)
+                .pipe(|x: SyncJson| x.version);
+            let toml_version = include_str!($path)
+                .pipe(toml::from_str::<CargoManifest>)
+                .pipe(Result::unwrap)
+                .pipe(|x: CargoManifest| x.package.version);
+            assert_eq!(toml_version, json_version);
+        }
+    };
 }
 
-fn test_against(toml_text: &str) {
-    let json_version = get_json_version();
-    let toml_version = toml_text
-        .pipe(toml::from_str::<CargoManifest>)
-        .pipe(Result::unwrap)
-        .pipe(|x: CargoManifest| x.package.version);
-    assert_eq!(toml_version, json_version);
-}
-
-#[test]
-fn bin_version() {
-    test_against(include_str!("../Cargo.toml"));
-}
-
-#[test]
-fn lib_version() {
-    test_against(include_str!("../../pretty-exec-lib/Cargo.toml"));
-}
+test_version!(bin_version, "../Cargo.toml");
+test_version!(lib_version, "../../pretty-exec-lib/Cargo.toml");
