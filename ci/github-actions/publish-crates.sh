3c512ce53c9fc1da9cf59dff0ab5b7984327e0f6
khai96_ <hvksmr1996@gmail.com>
2020-05-26 11:28:48 +0700

    Wait for version to be available

--- ci/github-actions/publish-crates.sh
+++ ci/github-actions/publish-crates.sh
@@ -1,10 +1,33 @@
 #! /bin/sh
 set -o errexit -o nounset
 
+if [ -z "$RELEASE_TAG" ]; then
+  echo '::error::Environment variable RELEASE_TAG is required but missing'
+  exit 1
+fi
+
+wait_for_version() (
+  sleep 1
+  printf '.'
+  url=https://raw.githubusercontent.com/rust-lang/crates.io-index/master/pr/et/pretty-exec-lib
+  curl -fsSL $url | while read -r json
+  do
+    tag=$(echo "$json" | jq --raw-output '.vers')
+    if [ "$tag" = "$RELEASE_TAG" ]; then
+      exit 0
+    fi
+  done
+  wait_for_version
+)
+
 publish() (
   echo "::group::Publishing $1..."
   cd ./crates/"$1"
   cargo publish
+  echo '::endgroup::'
+  echo "::group::Waiting for $1 to be available..."
+  wait_for_version
+  echo
   echo '::endgroup::'
 )
 
