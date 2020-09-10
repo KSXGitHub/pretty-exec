42b3896d7386c0640ad3cc0d8090e8cc3861a1b1
khai96_ <hvksmr1996@gmail.com>
2020-09-10 09:08:29 +0700

    Generate completion files

---
+++ exports/completion.bash
@@ -0,0 +1,45 @@
+_pretty-exec() {
+    local i cur prev opts cmds
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    cmd=""
+    opts=""
+
+    for i in ${COMP_WORDS[@]}
+    do
+        case "${i}" in
+            pretty-exec)
+                cmd="pretty-exec"
+                ;;
+            
+            *)
+                ;;
+        esac
+    done
+
+    case "${cmd}" in
+        pretty-exec)
+            opts=" -h -V  --skip-exec --github-actions --help --version --color  <program> <arguments>... "
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                
+                --color)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+        
+    esac
+}
+
+complete -F _pretty-exec -o bashdefault -o default pretty-exec
