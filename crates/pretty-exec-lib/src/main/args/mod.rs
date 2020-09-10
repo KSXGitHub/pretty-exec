9fd7688cbad1bf6ce38b92bc3c5ce5d4c64eaa43
khai96_ <hvksmr1996@gmail.com>
2020-09-10 18:49:11 +0700

    Use StructOptUtils::strict_from_args

--- crates/pretty-exec-lib/src/main/args/mod.rs
+++ crates/pretty-exec-lib/src/main/args/mod.rs
@@ -46,21 +46,4 @@ impl Args {
             syntax_highlight: self.syntax_highlight(),
         }
     }
-
-    pub fn get(args: impl Iterator<Item = String>) -> Self {
-        use std::process::exit;
-        match Self::from_iter_safe(args) as Result<Self, clap::Error> {
-            Ok(value) => value,
-            Err(clap::Error { kind, message, .. }) => match kind {
-                clap::ErrorKind::HelpDisplayed | clap::ErrorKind::VersionDisplayed => {
-                    println!("{}", message);
-                    exit(0);
-                }
-                _ => {
-                    println!("{}", message);
-                    exit(1);
-                }
-            },
-        }
-    }
 }
