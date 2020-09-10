6352d5db51081843fc765992d1a90e68958184a1
khai96_ <hvksmr1996@gmail.com>
2020-09-10 10:01:11 +0700

    Add tests to make sure completions are up-to-date

--- crates/pretty-exec/tests/sync.rs
+++ crates/pretty-exec/tests/sync.rs
@@ -1,4 +1,5 @@
 use pipe_trait::*;
+use pretty_exec::{args::Args, clap::Shell, structopt::StructOpt};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -35,3 +36,42 @@ macro_rules! test_version {
 
 test_version!(bin_version, "../Cargo.toml");
 test_version!(lib_version, "../../pretty-exec-lib/Cargo.toml");
+
+macro_rules! test_completion {
+    ($test_name:ident, $shell:expr, $path:literal) => {
+        #[test]
+        fn $test_name() {
+            let expected: &[u8] = include_bytes!($path);
+            let mut actual = Vec::new();
+            Args::clap().gen_completions_to("pretty-exec", $shell, &mut actual);
+            let actual = actual.as_slice();
+            assert_eq!(actual, expected);
+        }
+    };
+}
+
+test_completion!(
+    bash_completion,
+    Shell::Bash,
+    "../../../exports/completion.bash"
+);
+test_completion!(
+    fish_completion,
+    Shell::Fish,
+    "../../../exports/completion.fish"
+);
+test_completion!(
+    zsh_completion,
+    Shell::Zsh,
+    "../../../exports/completion.zsh"
+);
+test_completion!(
+    powershell_completion,
+    Shell::PowerShell,
+    "../../../exports/completion.ps1"
+);
+test_completion!(
+    elvish_completion,
+    Shell::Elvish,
+    "../../../exports/completion.elv"
+);
