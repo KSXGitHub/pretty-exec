f152276dd91cbffcfd9e80795cf6561f8baf233e
khai96_ <hvksmr1996@gmail.com>
2022-08-12 16:18:28 +0700

    refactor: Add some spaces

--- crates/pretty-exec-lib/src/log/github_actions.rs
+++ crates/pretty-exec-lib/src/log/github_actions.rs
@@ -3,12 +3,14 @@ use derive_more::From;
 
 #[derive(From)]
 pub struct GroupOpening<Fmt>(Fmt);
+
 impl<Fmt: Format> Format for GroupOpening<Fmt> {
     type Output = String;
     fn fmt(&self, program: impl AsRef<OsStr>, arguments: &[impl AsRef<OsStr>]) -> String {
         format!("::group::{}", self.0.fmt(program, arguments))
     }
 }
+
 impl<Fmt: Format> Log for GroupOpening<Fmt> {
     fn log(&self, program: impl AsRef<OsStr>, arguments: &[impl AsRef<OsStr>]) {
         println!("{}", self.fmt(program, arguments))
@@ -16,12 +18,14 @@ impl<Fmt: Format> Log for GroupOpening<Fmt> {
 }
 
 pub struct GroupClosing;
+
 impl Format for GroupClosing {
     type Output = &'static str;
     fn fmt(&self, _: impl AsRef<OsStr>, _: &[impl AsRef<OsStr>]) -> &'static str {
         "::endgroup::"
     }
 }
+
 impl Log for GroupClosing {
     fn log(&self, program: impl AsRef<OsStr>, arguments: &[impl AsRef<OsStr>]) {
         println!("{}", self.fmt(program, arguments))
--- crates/pretty-exec-lib/src/log/nothing.rs
+++ crates/pretty-exec-lib/src/log/nothing.rs
@@ -1,5 +1,7 @@
 use super::{Log, OsStr};
+
 pub struct Nothing;
+
 impl Log for Nothing {
     fn log(&self, _: impl AsRef<OsStr>, _: &[impl AsRef<OsStr>]) {}
 }
