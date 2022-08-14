7048a4084be04f8da6ef63c05db97f8a1f69e5c1
khai96_ <hvksmr1996@gmail.com>
2022-08-14 16:02:45 +0700

    api: Reduce copying in `PrettyExec`

--- crates/pretty-exec-lib/src/pretty_exec.rs
+++ crates/pretty-exec-lib/src/pretty_exec.rs
@@ -4,46 +4,34 @@ use std::{
     process::{Command, ExitStatus},
 };
 
-pub struct PrettyExec<PreLog, PostLog> {
-    prompt: String,
-    program: String,
-    arguments: Vec<String>,
+pub struct PrettyExec<'a, PreLog, PostLog> {
+    prompt: &'a str,
+    program: &'a str,
+    arguments: Vec<&'a str>,
     command: Command,
     log_before: PreLog,
     log_after: PostLog,
 }
 
-impl<PreLog, PostLog> PrettyExec<PreLog, PostLog> {
-    pub fn arg(&mut self, arg: &str) -> &mut Self {
-        self.arguments.push(arg.to_owned());
+impl<'a, PreLog, PostLog> PrettyExec<'a, PreLog, PostLog> {
+    pub fn arg(&mut self, arg: &'a str) -> &mut Self {
+        self.arguments.push(arg);
         self.command.arg(arg);
         self
     }
 
-    pub fn spawn<'a>(&'a mut self) -> io::Result<ExitStatus>
+    pub fn spawn(&'a mut self) -> io::Result<ExitStatus>
     where
-        Logger<'a, PreLog, String, String, Vec<String>>: Log,
-        Logger<'a, PostLog, String, String, Vec<String>>: Log,
+        Logger<'a, PreLog, str, str, Vec<&'a str>>: Log,
+        Logger<'a, PostLog, str, str, Vec<&'a str>>: Log,
     {
-        Logger::new(
-            &self.log_before,
-            &self.prompt,
-            &self.program,
-            &self.arguments,
-        )
-        .log();
+        Logger::new(&self.log_before, self.prompt, self.program, &self.arguments).log();
         let result = self.command.spawn()?.wait();
-        Logger::new(
-            &self.log_after,
-            &self.prompt,
-            &self.program,
-            &self.arguments,
-        )
-        .log();
+        Logger::new(&self.log_after, self.prompt, self.program, &self.arguments).log();
         result
     }
 
-    pub fn set_log_before<Logger>(self, log_before: Logger) -> PrettyExec<Logger, PostLog> {
+    pub fn set_log_before<Logger>(self, log_before: Logger) -> PrettyExec<'a, Logger, PostLog> {
         PrettyExec {
             prompt: self.prompt,
             program: self.program,
@@ -54,7 +42,7 @@ impl<PreLog, PostLog> PrettyExec<PreLog, PostLog> {
         }
     }
 
-    pub fn set_log_after<Logger>(self, log_after: Logger) -> PrettyExec<PreLog, Logger> {
+    pub fn set_log_after<Logger>(self, log_after: Logger) -> PrettyExec<'a, PreLog, Logger> {
         PrettyExec {
             prompt: self.prompt,
             program: self.program,
@@ -66,11 +54,11 @@ impl<PreLog, PostLog> PrettyExec<PreLog, PostLog> {
     }
 }
 
-impl PrettyExec<Nothing, Nothing> {
-    pub fn new(prompt: &str, program: &str) -> Self {
+impl<'a> PrettyExec<'a, Nothing, Nothing> {
+    pub fn new(prompt: &'a str, program: &'a str) -> Self {
         PrettyExec {
-            prompt: prompt.to_owned(),
-            program: program.to_owned(),
+            prompt,
+            program,
             log_before: Nothing,
             log_after: Nothing,
             arguments: Vec::new(),
