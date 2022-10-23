1bff2c778e3b35988d676d0deb97dcde9a2f1aca
khai96_ <hvksmr1996@gmail.com>
2022-10-23 22:13:16 +0700

    refactor: Remove unnecessary `?`

--- crates/pretty-exec-lib/src/app/exec.rs
+++ crates/pretty-exec-lib/src/app/exec.rs
@@ -11,16 +11,16 @@ pub fn exec(param: Param) -> Result<ExitStatus, Error> {
     } = param;
     let pretty_exec = PrettyExec::new(prompt, program, arguments);
 
-    Ok(if support_github_action {
+    if support_github_action {
         pretty_exec
             .set_log_before(github_actions::GroupOpening::from(syntax_highlight))
             .set_log_after(github_actions::GroupClosing)
             .spawn()
-            .map_err(Error::ExecutionError)?
+            .map_err(Error::ExecutionError)
     } else {
         pretty_exec
             .set_log_before(syntax_highlight)
             .spawn()
-            .map_err(Error::ExecutionError)?
-    })
+            .map_err(Error::ExecutionError)
+    }
 }
